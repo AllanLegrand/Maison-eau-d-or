3e5bc8eddd53ee304c36f6f151b61e0461d21db5
Lorenzo <lorenzo.de-macedo@etu.univ-lehavre.fr>
2024-12-02 13:35:40 +0100

    bug

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,11 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-<<<<<<< HEAD
 $routes->get('/', 'Accueil::index');
-=======
 $routes->get('/', 'Home::index');
 
 $routes->get('/forgot-password', 'ForgotPasswordController::index'); 
 $routes->post('/forgot-password/sendResetLink', 'ForgotPasswordController::sendResetLink');
->>>>>>> 6eab0dc6d80d55f578ae2b5b82bbd2c861e1bd84
